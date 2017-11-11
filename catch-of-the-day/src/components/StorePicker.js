66c1c2c02123edae365f286a1c68573357a4e24a
Raymond Gan <rayning@yahoo.com>
2017-11-11 08:26:18 -0800

    Make empty Header, Order, and Inventory React components

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    // comment outside JSX
     return (
       <form className="store-selector">
-        { /* Comment in JSX */ }
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name" />
         <button type="submit">Visit Store →</button>
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './css/style.css';
+import App from './components/App';
 
 import StorePicker from './components/StorePicker';
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
