30ba6a979bd3b9bc9abfdea23a0c99e21ebe54ee
Raymond Gan <rayning@yahoo.com>
2017-11-13 17:46:48 -0800

    React Events: adds "ref" and goToStore event, with arrow functions
    - Adds ref to make "storeInput" property and set it to "input" HTML tag
    - Change this.goToStore in form to ES6 arrow function, which sets "this" to StorePicker (by LEXICAL SCOPE) and doesn't require binding
    - This gives us access to "this" in goToStore() and lets us print value of store name

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,30 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-  render() {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  goToStore(event) {
+    event.preventDefault(); // stop page from reloading
+    console.log("You changed the URL");
+    // grab text from textbox
+    console.log(this.storeInput.value);
+    // change route from / to /store/:storeId
+  }
+
+  render() {  //render() is bound to the component
     return (
-      <form className="store-selector">
+      // This line defines "this" as StorePicker and binds goToStore() to "this"
+      // <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+
+      // OR this ES6 arrow function follows LEXICAL SCOPE (automatically setting
+      // "this" to StorePicker), with NO need for binding
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+
+      { /* This "ref" make "storeInput" property and sets it to "input" HTML tag */ }
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} />
         <button type="submit">Visit Store →</button>
       </form>
     )
