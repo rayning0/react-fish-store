ec407a06957e5b123240c418066c201aaffada86
Raymond Gan <rayning@yahoo.com>
2017-11-18 23:59:56 -0800

    React state: make AddFishForm and related changes to add new fish to App.state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,32 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this); //makes addFish() a method on App
+
+    // getinitialState
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // update state
+    //   this.state.fishes.fish1 = fish; <--can do this, but don't
+    //   Spread Attributes: if you have props as an object and want to pass it in JSX, you can use ... as a “spread” operator to pass whole props object.
+    const fishes = {...this.state.fishes}; //copy existing state into "fishes"
+
+    // add our new fish
+    const timestamp = Date.now(); // ms since the UNIX epoch (1/1/1970)
+    fishes[`fish-${timestamp}`] = fish;
+
+    // set state. Updates App.state
+    this.setState({ fishes }); //shortcut for line below
+    // this.setState({ fishes: fishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +37,8 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+      { /* pass addFish() to Inventory */ }
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
   render() {
     return (
-      <p>Inventory</p>
+      <div>
+        <h2>Inventory</h2>
+      { /* Pass addFish() to AddFishForm with props */ }
+        <AddFishForm addFish={this.props.addFish} />
+      </div>
     )
   }
 }
