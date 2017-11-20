af46c7ccaa16f8844e5e30b3f01208cac7ca24c5
Raymond Gan <rayning@yahoo.com>
2017-11-20 08:37:33 -0800

    Save details of each fish on Firebase, with re-base (https://github.com/tylermcginnis/re-base)

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCvgd6dthExAxTWbpMcEXET02vMIuNGATQ",
+  authDomain: "catch-of-the-day-rgan.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-rgan.firebaseio.com"
+});
+
+export default base;
