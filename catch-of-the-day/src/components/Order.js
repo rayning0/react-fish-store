532bb178b879268ef2fd26b5e65623c61bdddb35
Raymond Gan <rayning@yahoo.com>
2017-11-22 00:01:08 -0800

    Horizontally CSS animate adding/removing an order. Vertically animate # of lbs as # increases.

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { formatPrice } from '../helpers';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Order extends React.Component {
   constructor() {
@@ -18,7 +19,19 @@ class Order extends React.Component {
 
     return (
       <li key={key}>
-        <span>{count} lb{count > 1 ? 's' : ''} {fish.name} {removeButton}</span>
+        <span>
+          <CSSTransitionGroup
+            component="span"
+            className="count"
+            transitionName="count"
+            transitionEnterTimeout={250}
+            transitionLeaveTimeout={250}
+          >
+            <span key={count}>{count}</span>
+          </CSSTransitionGroup>
+
+          lb{count > 1 ? 's' : ''} {fish.name} {removeButton}
+        </span>
         <span className="price">{formatPrice(count * fish.price)}</span>
       </li>
     )
@@ -38,13 +51,21 @@ class Order extends React.Component {
     return (
       <div className="order-wrap">
         <h2>Your Order</h2>
-        <ul className="order">
+
+        <CSSTransitionGroup
+          className="order"
+          component="ul"
+          transitionName="order"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+        >
           {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Total:</strong>
             {formatPrice(total)}
           </li>
-        </ul>
+        </CSSTransitionGroup>
+
 
       </div>
     )
